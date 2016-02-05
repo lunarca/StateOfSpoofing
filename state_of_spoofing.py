96dcccba9a0945de31ca0c30e22b9e972a2a8040
Alex DeFreese <alexdefreese@gmail.com>
2016-02-04 23:04:17 -0500

    Initialize database in main file

--- state_of_spoofing.py
+++ state_of_spoofing.py
@@ -1,9 +1,28 @@
+#! /usr/bin/env python
+
 import dnslib
+import argparse
+import os
+
 from libs.subbrute import resolver
+
+from models.domain import Domain, Base
+
+from sqlalchemy import create_engine
 
 
 def main():
-    pass
+    args = parse_args()
+
+    engine = create_engine("sqlite:///%(db)s" % {'db': args.db})
+    Base.metadata.create_all(engine)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("domain_filename", help="Filename containing domains to scan")
+    parser.add_argument("--db", type=str, help="Database file name", default="state.db")
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
